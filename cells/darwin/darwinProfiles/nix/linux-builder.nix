e7be8729d6638f2b17e6fa9580b941129c03b4c5
Alex Zero <me@alex0.net>
2023-07-20 07:20:32 +0100

    fix: fix stuff

---
+++ cells/darwin/darwinProfiles/nix/linux-builder.nix
@@ -0,0 +1,111 @@
+{ root, inputs, cell, ... }:
+{ self, config, lib, pkgs, ... }:
+let
+  inherit (inputs) nixpkgs nixos-stable;
+  inherit (nixos-stable.lib) nixosSystem;
+  cfg = config.rebar;
+
+  inherit (nixpkgs) system;
+  linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] system;
+
+  # using nixos-unstable as this is a relatively new feature
+  builder = nixosSystem {
+    system = linuxSystem;
+    modules = [
+      "${nixos-stable}/nixos/modules/profiles/macos-builder.nix"
+      {
+        system.nixos.revision = lib.mkForce null;
+        virtualisation = {
+          host.pkgs = nixos-stable.legacyPackages."${system}";
+          forwardPorts = lib.mkForce [{
+            from = "host";
+            host.address = "127.0.0.1";
+            host.port = cfg.darwin.linux-builder.hostPort;
+            guest.port = 22;
+          }];
+        };
+      }
+    ];
+  };
+
+  # A Bash script for running the builder
+  runLinuxBuilderScript = pkgs.writeShellScriptBin "run-linux-builder" ''
+    set -uo pipefail
+    trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+    IFS=$'\n\t'
+    mkdir -p "${cfg.darwin.linux-builder.dataDir}"
+    cd "${cfg.darwin.linux-builder.dataDir}"
+    ${builder.config.system.build.macos-builder-installer}/bin/create-builder
+  '';
+in {
+  options.rebar.darwin.linux-builder = {
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/linux-builder";
+      description = "Directory to store the linux builder state";
+    };
+
+    logPath = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/log/linux-builder.log";
+      description = "Path to store the linux builder log";
+    };
+
+    hostPort = lib.mkOption {
+      type = lib.types.int;
+      default = 31022;
+      description = "Port to use for the local builder";
+    };
+
+    maxJobs = lib.mkOption {
+      type = lib.types.int;
+      default = 4;
+      description = "Number of jobs to run in parallel on the local builder";
+    };
+
+    speedFactor = lib.mkOption {
+      type = lib.types.int;
+      default = 1;
+      description = "Speed factor to use for the local builder";
+    };
+  };
+
+  config = {
+    programs.ssh.knownHosts = {
+      "linux-builder.localhost" = {
+        hostNames = [ "linux-builder.localhost" ];
+        publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBWcxb/Blaqt1auOtE+F8QUWrUotiC5qBJ+UuEWdVCb";
+      };
+    };
+
+    environment.etc."ssh/ssh_config.d/builder-localhost".text = ''
+      Host linux-builder.localhost
+        User builder
+        HostName 127.0.0.1
+        Port ${toString cfg.darwin.linux-builder.hostPort}
+        IdentityFile /etc/nix/builder_ed25519
+    '';
+
+    nix = {
+      distributedBuilds = true;
+      buildMachines = [{
+        inherit (cfg.darwin.linux-builder) maxJobs speedFactor;
+        hostName = "linux-builder.localhost";
+        system = linuxSystem;
+        supportedFeatures = [ "kvm" "benchmark" "big-parallel" "nixos-test" ];
+      }];
+    };
+
+    launchd.daemons.linux-builder = {
+      command = "${runLinuxBuilderScript}/bin/run-linux-builder";
+      path = with pkgs; [ "/usr/bin" coreutils nix ];
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = cfg.darwin.linux-builder.logPath;
+        StandardErrorPath = cfg.darwin.linux-builder.logPath;
+      };
+    };
+  };
+}
--- cells/nixos/checks.nix
+++ cells/nixos/checks.nix
@@ -1,12 +1,13 @@
 _inputs@{ cell, inputs, }:
 let
   pkgs = inputs.nixpkgs;
-  # inherit (inputs) hive self;
+  inherit (inputs) hive self;
   inherit (pkgs) lib system;
 
   nixosLib = import "${pkgs.path}/nixos/lib" { inherit system pkgs; };
 
   moduleTests = {
+    # this test actually runs the server module in a virtual machine
     server = nixosLib.runTest {
       name = "server";
       hostPkgs = pkgs;
@@ -24,15 +25,14 @@ let
   };
 
   # Only check the configurations for the current system
-  # sysConfigs = let
-  #   nixosConfigurations = hive.collect self "nixosConfigurations";
-  # in lib.filterAttrs (_name: value: value.pkgs.system == system) nixosConfigurations;
+  sysConfigs =
+    let nixosConfigurations = hive.collect self "nixosConfigurations";
+    in lib.filterAttrs (_name: value: value.pkgs.system == system)
+    nixosConfigurations;
 
   # Add all the nixos configurations to the checks
-  # nixosChecks =
-  #   lib.mapAttrs'
-  #     (name: value: { name = "nixos-${name}"; value = value.config.system.build.toplevel; })
-  #     sysConfigs;
-
-  nixosChecks = { };
+  nixosChecks = lib.mapAttrs' (name: value: {
+    name = "nixos-${name}";
+    value = value.config.system.build.toplevel;
+  }) sysConfigs;
 in nixosChecks // (lib.optionalAttrs pkgs.stdenv.isLinux moduleTests)
